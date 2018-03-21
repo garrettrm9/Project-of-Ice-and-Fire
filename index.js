79a9241f60f59a6a570d8626e9b2e23e50800d14
Garrett Rasko-Martinis <garrettrm9@gmail.com>
2018-03-21 01:37:22 -0400

    set up root index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mustacheExpress = require("mustache-express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.engine("html", mustacheExpress());
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((err, req, res, next) => {
+  console.log("Error encountered:", err);
+  res.status(500);
+  res.send(err);
+});
+
+app.listen(PORT, () => {
+  console.log("Server started on " + PORT);
+});
